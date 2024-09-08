7c6c514b0a5ae8e67a75f34146435d6f74315538
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-08 12:35:50 +0300

    js-lab

---
+++ main.js
@@ -0,0 +1,62 @@
+
+
+// حساب مساحة المستطيل
+let width = 10
+let length = 20
+let x = width + length
+console.log("The area of the rectangle is:" + x);
+//محول درجة الحرارة
+
+let temperature=40;
+let Fahrenheit =(temperature*9/5) +32;
+let celsius=(Fahrenheit-32)*5/9;
+console.log(`The temperature ${Fahrenheit} °F is ${celsius.toFixed(2)} degrees celsius
+the temperature ${celsius}  °C is ${Fahrenheit} degrees Fahrenheit` );
+
+
+//من ساعه الى ثانيه
+
+
+let y = 3
+let z = 60 * 60
+console.log(y + "hours = " + z + "Seconds");
+
+
+
+//Concatenation
+
+let thingOne = "Ghaida";
+
+
+let thingTwo =" Aloufi" 
+console.log( thingOne+  thingTwo );
+
+
+//Total bill
+
+var bill = 10.25 + 3.99 + 7.15
+var tip = bill * 0.15
+var total = bill + tip
+console.log(total);
+
+
+
+//Mad Libs
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = 'The Intro to JavaScript course is ' + adjective1 + '. James and Julia are so' + adjective2 + '. I cannot wait to work through the rest of this ' + adjective3 + ' .content!'
+
+console.log(madLib);
+
+
+//Awesome Message
+
+
+let firstName = "Julia"
+let interest = "cats"
+let hoby="play video games"
+ let awesomeMessage= "Hi, my name is "+firstName+".I love "+interest+".In my spare time, I like to "+hoby+ "."
+ console.log(awesomeMessage);
+ 
